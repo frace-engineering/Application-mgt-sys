c64df2d18d6bd2dbe46370904b1cfff8ea04347f
Engr Ugwu <fugwungene@gmail.com>
2024-04-26 22:46:22 +0000

    Add virtualenv.

--- appoms.py
+++ appoms.py
@@ -338,4 +338,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
---
+++ environment_variables.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+export APPOMS_MYSQL_USER=appoms_dev
+export APPOMS_MYSQL_DB=appo_mgt_sys
+export APPOMS_MYSQL_HOST=localhost
+export APPOMS_MYSQL_PWD=appoms_dev_pwd
